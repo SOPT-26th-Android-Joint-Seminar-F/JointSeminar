ce979552d46d3d5abc16cc1568e1e2cbdad91900
HyeranShin <hyeran9712@naver.com>
2020-05-25 18:32:44 +0900

    add: bottom bar, activity-fragment 연결

--- app/src/main/java/com/hyeran/jointseminar/MainActivity.kt
+++ app/src/main/java/com/hyeran/jointseminar/MainActivity.kt
@@ -2,11 +2,20 @@ package com.hyeran.jointseminar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        addFragment(HomeFragment())
+    }
+
+    private fun addFragment(fragment: Fragment) {
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.framelayout_main, HomeFragment()).commitAllowingStateLoss()
     }
 }
