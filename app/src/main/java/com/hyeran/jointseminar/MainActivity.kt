7df377960149ff32394cbb9eadf284a5f09cf87c
SONPYEONGHWA <ssrsph@naver.com>
2020-05-29 03:33:44 +0900

    fragment_home 포스터 부분

--- app/src/main/java/com/hyeran/jointseminar/MainActivity.kt
+++ app/src/main/java/com/hyeran/jointseminar/MainActivity.kt
@@ -4,18 +4,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         addFragment(HomeFragment())
+
     }
 
     private fun addFragment(fragment: Fragment) {
+
         val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.framelayout_main, HomeFragment()).commitAllowingStateLoss()
+
     }
 }
