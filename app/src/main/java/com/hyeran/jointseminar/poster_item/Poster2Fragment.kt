9882310b7f438d9aa0d643b082db7f8011b60c94
SONPYEONGHWA <ssrsph@naver.com>
2020-05-30 00:00:50 +0900

    평화 수정본

---
+++ app/src/main/java/com/hyeran/jointseminar/poster_item/Poster2Fragment.kt
@@ -0,0 +1,34 @@
+package com.hyeran.jointseminar.poster_item
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import com.hyeran.jointseminar.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class Poster2Fragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_poster2, container, false)
+
+        var view = layoutInflater!!.inflate(R.layout.fragment_poster2, container, false)
+        var imageView = view.findViewById<ImageView>(R.id.iv_poster2)
+
+        if(arguments != null) {
+            val args = arguments
+            imageView.setImageResource(args!!.getInt("imgRes"))
+        }
+
+        return  view
+    }
+
+}
