098a5f8d52efcfe63ae991fb9bbc7da885b0cf7e
jooyae <jooyae3970@naver.com>
2020-05-30 15:45:02 +0900

    contentsviewfragmentjy_4

---
+++ app/src/main/java/com/hyeran/jointseminar/MainPagerAdapter.kt
@@ -0,0 +1,17 @@
+package com.hyeran.jointseminar
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class MainPagerAdapter(fm: FragmentManager):
+    FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT){
+    override fun getItem(position: Int):Fragment{
+        return when (position){
+            0->HomeFragment()
+            else->ContentFragment()
+        }
+    }
+
+    override fun getCount()= 2
+}
